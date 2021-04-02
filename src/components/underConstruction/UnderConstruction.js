7b70c8f7b2c40d6af6612b320ec58a4dc4196682
Sridhar Muraleedar <sridharmuraleedar@Sridhars-MacBook-Pro.local>
2021-04-02 22:19:28 +0530

    basic covered

--- src/components/underConstruction/UnderConstruction.js
+++ src/components/underConstruction/UnderConstruction.js
@@ -1,28 +1,34 @@
 import pcvideo from "../../assets/creaweb.mp4";
 import mobilevideo from "../../assets/final whatsapp.mp4";
 import crea from "../../assets/crea move.mp4"
-import "./UnderConstruction.css"
+// import "./UnderConstruction.css"
+import "./uc.css"
+import facebook from "../../assets/fB.png"
+import instagram from "../../assets/insta.png"
+import pinterest from "../../assets/pin.png"
+import tumblr from "../../assets/tumblr.png"
+import youtube from "../../assets/youtube.png"
 
 function UnderConstruction() {
     return (
-        <div>
-        <div class="videoContainer">
-            <ul>
-                <li><a href="/site" class="btn-liquid"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png"/><span></span></a></li>
-                <li><a href="/site" class="btn-liquid"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png"/></a></li>
-                <li><a href="/site" class="btn-liquid"><img src="https://img.icons8.com/bubbles/100/000000/pinterest.png"/></a></li>
-                <li><a href="/site" class="btn-liquid"><img src="https://img.icons8.com/bubbles/100/000000/apple-mail.png"/></a></li>
-                <li><a href="/site" class="btn-liquid"><img src="https://img.icons8.com/bubbles/100/000000/linkedin.png"/></a></li>
-            </ul>
-
-        <video class="pc-video" autoPlay playsInline preload="auto" muted>
-            <source src={crea} type="video/mp4"/>
-        </video>
-        </div>
-        <div class="rotateScreen">
-            <span>Please rotate your screen for best experience</span>
-            </div>
-            </div>
+<div>
+   <div class="social-container">
+       <span class="social-reach">Connect with us through..</span>
+      <ul class="social-icons">
+         <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+         <li><a href="#"><i class="fa fa-tumblr"></i></a></li>
+         <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+         <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+         <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+      </ul>
+      {/* <video class="pc-video" autoPlay playsInline preload="auto" muted>
+         <source src={crea} type="video/mp4"/>
+      </video> */}
+   </div>
+   <div class="rotateScreen">
+      <span>Please rotate your screen for best experience</span>
+   </div>
+</div>
     );
 }
 

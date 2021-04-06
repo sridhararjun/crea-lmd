7a1cf65eeb5c3d187f46a704f1382c191b2710d3
Sridhar Arjun <sridharmuraleedar@gmail.com>
2021-04-06 23:16:56 +0530

    1)on hover extend completed and working,2)Icon animation works on end of vide only

--- src/components/underConstruction/UnderConstruction.js
+++ src/components/underConstruction/UnderConstruction.js
@@ -1,29 +1,7 @@
-import pcvideo from "../../assets/creaweb.mp4";
-import mobilevideo from "../../assets/final whatsapp.mp4";
 import crea from "../../assets/crea move.mp4"
-// import "./UnderConstruction.css"
 import "./uc.css"
-import facebook from "../../assets/fB.png"
-import instagram from "../../assets/insta.png"
-import pinterest from "../../assets/pin.png"
-import tumblr from "../../assets/tumblr.png"
-import youtube from "../../assets/youtube.png"
-import audio_crea from "../../assets/crea-audio.mp3"
+import rotateScreen from "../../assets/rotate-1.png"
 
-// window.onload = function(){
-//     var els = document.getElementsByClassName("play-link");
-//     var creaVid = document.getElementsByClassName("pc-video");
-//     els.addEventListener('click', function(e) {
-//         e.target.previousElementSibling.classList.add('play');
-//         return false;
-//     });
-//     creaVid.addEventListener("click",function(e){
-//         if(creaVid.paused==true){
-//             creaVid.play();
-//         }
-//     });
-    
-// }
 
 function UnderConstruction() {
     function handleClick(e){
@@ -32,12 +10,15 @@ function UnderConstruction() {
         var reach = document.getElementById("reach");
         var animatedIconsList = document.getElementById("icons").getElementsByTagName("li");
         var toBeRemoved = document.getElementById("btn");
-        for(var i=0;i<animatedIconsList.length;i++){
-            animatedIconsList[i].classList.add("play");
-        }
-        reach.classList.add("play");
+        
         creaVid.play();
         toBeRemoved.remove();
+        creaVid.addEventListener("ended", function() {
+            for (var i = 0; i < animatedIconsList.length; i++) {
+                animatedIconsList[i].classList.add("play");
+            }
+            reach.classList.add("play");
+        })
         
     }
     return (
@@ -45,20 +26,58 @@ function UnderConstruction() {
    <div class="social-container">
       <span id="reach" class="social-reach">Connect with us through..</span>
       <ul id="icons" class="social-icons">
-         <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-         <li><a href="#"><i class="fa fa-envelope"></i></a></li>
-         <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-         <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-         <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+         <li class="instagram">
+            <a href="#">
+               <i class="fa fa-instagram"></i>
+               <div class="slider">
+                  <p>instagram</p>
+               </div>
+            </a>
+         </li>
+         <li class="mail">
+            <a href="#">
+               <i class="fa fa-envelope"></i>
+               <div class="slider">
+                  <p>mail</p>
+               </div>
+            </a>
+         </li>
+         <li class="linkedin">
+            <a href="#">
+               <i class="fa fa-linkedin"></i>
+               <div class="slider">
+                  <p>linkedin</p>
+               </div>
+            </a>
+         </li>
+         <li class="facebook">
+            <a href="#">
+               <i class="fa fa-facebook"></i>
+               <div class="slider">
+                  <p>facebook</p>
+               </div>
+            </a>
+         </li>
+         <li class="pinterest">
+            <a href="#">
+               <i class="fa fa-pinterest"></i>
+               <div class="slider">
+                  <p>pinterest</p>
+               </div>
+            </a>
+         </li>
       </ul>
       {/* <a href="#" onClick={handleClick} id="play-link">Play</a> */}
-      <div id="btn"><span class="noselect"><a href="#" onClick={handleClick} id="play-link">Play</a></span><div id="circle"></div></div>
+      <div id="btn">
+         <span class="noselect"><a href="#" onClick={handleClick} id="play-link">Play</a></span>
+         <div id="circle"></div>
+      </div>
       <video id="video" class="pc-video" playsInline>
          <source src={crea} type="video/mp4"/>
       </video>
    </div>
    <div class="rotateScreen">
-      <span>Please rotate your screen for best experience</span>
+      <img src={rotateScreen} alt=""/>
    </div>
 </div>
     );
